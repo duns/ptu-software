b131a94134e82ddc2a56cb62fb99d16a1f7ef6cf
Kostas Tzevanidis <ktzevanid@gmail.com>
2012-11-09 16:16:26 +0200

    Execution script optimized.

--- src/videosource/scripts/videosource.sh
+++ src/videosource/scripts/videosource.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-SOURCEBIN=videosource
+SOURCEBIN=/home/root/workspace/ptu-software/bin/videosource
 CFGFILE=/etc/videostream.conf/vsource_cfg
-
-${SOURCEBIN} --config-path ${CFGFILE} &
-VSPID=$!
 
 control_c()
 {
-	while [ `ps --no-headers -p ${VSPID} -o "%p" | awk '{print $1}'` ]
-	do
-		kill ${VSPID} 2> /dev/null
-	done
 	exit 0
 }
 
@@ -19,11 +12,6 @@ trap control_c SIGINT
 
 while true
 do
-	if [ ! `ps --no-headers -p ${VSPID} -o "%p" | awk '{print $1}'` ] ; then
-		${SOURCEBIN} --config-path ${CFGFILE} &
-		VSPID=$!	
-	fi
-	
-	sleep 5 
+	${SOURCEBIN} --config-path ${CFGFILE}
 done
 
